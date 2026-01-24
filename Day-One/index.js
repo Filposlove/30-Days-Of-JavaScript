f0365d6c5f9fbd832e4c43dbdf1436890acc6b70
filpos <mergetafilpos@gmail.com>
2026-01-24 17:09:26 +0300

    day one index message

---
+++ Day-One/index.js
@@ -0,0 +1,18 @@
+/*let a = 7;
+b = 8;
+console.log(b-a);
+let studenOfJeadan = 10;
+console.log(studenOfJeadan);
+*/
+
+let mom = 80;
+if(mom <=60){
+  console.log("were is mom");
+}
+else if(mom == 70){
+  console.log("do you know mom");
+}
+else {
+  console.log("i love mom")
+}
+
