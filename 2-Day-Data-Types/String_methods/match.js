75f9cde1f35cd8509dcc5f61969cd8b1ca4e9034
filpos <mergetafilpos@gmail.com>
2026-01-25 18:41:50 +0300

    create 2_Day match.js message

--- 2-Day-Data-Types/String_methods/match.js
+++ 2-Day-Data-Types/String_methods/match.js
@@ -3,7 +3,7 @@ let string = "love"
 let patternOne = /love/ // with out any flag
 let patternTwo = /love/gi // g-means to search in the whole text, i - case insensitive
 string.match(substring)
-let string = 'I love JavaScript. If you do not love JavaScript what else can you love.'
+let string = "I love JavaScript. If you do not love JavaScript what else can you love."
 console.log(string.match('love')) //
 /*
 output
@@ -14,7 +14,7 @@ let pattern = /love/gi
 console.log(string.match(pattern)) // ["love", "love", "love"]
 // Let us extract numbers from text using regular expression. This is not regular expression section, no panic.
 
-let txt = 'In 2019, I run 30 Days of Python. Now, in 2020 I super exited to start this challenge'
+let txt = "In 2019, I run 30 Days of Python. Now, in 2020 I super exited to start this challenge"
 let regEx = /\d/g // d with escape character means d not a normal d instead acts a digit
 // + means one or more digit numbers, 
 // if there is g after that it means global, search everywhere.
