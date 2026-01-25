44f94adf921116f8f2a0752d4f89c03aa5ebdc12
filpos <mergetafilpos@gmail.com>
2026-01-25 17:20:45 +0300

    create 2_Day length.js message

---
+++ 2-Day-Data-Types/String_methods/length.js
@@ -0,0 +1,8 @@
+// length: The string length method returns the number of characters in a string included empty space. Example:
+
+let js = 'JavaScript'
+console.log(js.length)        // 10
+let firstName = 'Phillpos'
+console.log(firstName.length) // 8
+let age = "32";
+console.log(age.length)  // 2
