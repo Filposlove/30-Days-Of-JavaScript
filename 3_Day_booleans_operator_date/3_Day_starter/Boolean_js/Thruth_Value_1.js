8c9f61661ca4f7657fc273ca1a783df573363fba
filpos <mergetafilpos@gmail.com>
2026-01-26 19:12:52 +0300

    the boolean Truthly message

--- 3_Day_booleans_operator_date/3_Day_starter/Boolean_js/Thruth_Value_1.js
+++ 3_Day_booleans_operator_date/3_Day_starter/Boolean_js/Thruth_Value_1.js
@@ -1,3 +1,15 @@
+/*
+Operators
+Assignment operators
+An equal sign in JavaScript is an assignment operator. It uses to assign a variable.
+
+let firstName = 'Asabeneh'
+let country = 'Finland'
+Assignment Operators
+
+Assignment operators */
+
+
 let empty = null
 console.log(empty) // -> null , means no value
 
@@ -13,8 +25,8 @@ Division(/): a / b
 Modulus(%): a % b
 Exponential(**): a ** b */
 
-let numOne = 4;
-let numTwo = 3;
+let numOne = 6;
+let numTwo = 2;
 let sum = numOne + numTwo
 let diff = numOne - numTwo
 let mult = numOne * numTwo
@@ -22,4 +34,45 @@ let div = numOne / numTwo
 let remainder = numOne % numTwo
 let powerOf = numOne ** numTwo
 
-console.log(sum, diff, mult, div, remainder, powerOf) // 7,1,12,1.33,1, 64
+console.log(sum, diff, mult, div, remainder, powerOf) // "8 4 12 3 0 36"
+// this is other opition of length in mater
+const PI = 30
+let radius = 80          // length in meter
+
+//Let us calculate area of a circle
+const areaOfCircle = PI * radius / radius
+console.log(areaOfCircle)  //  30 m
+
+//comparation operator
+console.log(3 > 2)              // true, because 3 is greater than 2
+console.log(3 >= 2)             // true, because 3 is greater than 2
+console.log(3 < 2)              // false,  because 3 is greater than 2
+console.log(2 < 3)              // true, because 2 is less than 3
+console.log(2 <= 3)             // true, because 2 is less than 3
+console.log(3 == 2)             // false, because 3 is not equal to 2
+console.log(3 != 2)             // true, because 3 is not equal to 2
+console.log(3 == '3')           // true, compare only value
+console.log(3 === '3')          // false, compare both value and data type
+console.log(3 !== '3')          // true, compare both value and data type
+console.log(3 != 3)             // false, compare only value
+console.log(3 !== 3)            // false, compare both value and data type
+console.log(0 == false)         // true, equivalent
+console.log(0 === false)        // false, not exactly the same
+console.log(0 == '')            // true, equivalent
+console.log(0 == ' ')           // true, equivalent
+console.log(0 === '')           // false, not exactly the same
+console.log(1 == true)          // true, equivalent
+console.log(1 === true)         // false, not exactly the same
+console.log(undefined == null)  // true
+console.log(undefined === null) // false
+console.log(NaN == NaN)         // false, not equal
+console.log(NaN === NaN)        // false
+console.log(typeof NaN)         // number
+
+console.log('mango'.length == 'avocado'.length)  // false
+console.log('mango'.length != 'avocado'.length)  // true
+console.log('mango'.length < 'avocado'.length)   // true
+console.log('milk'.length == 'meat'.length)      // true
+console.log('milk'.length != 'meat'.length)      // false
+console.log('tomato'.length == 'potato'.length)  // true
+console.log('python'.length > 'dragon'.length)   // false
