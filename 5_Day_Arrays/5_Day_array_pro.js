73d7d7566faf009ab0da14237350c4ff383887f8
filpos <mergetafilpos@gmail.com>
2026-01-28 14:30:13 +0300

    create 5_day_array index.html message

---
+++ 5_Day_Arrays/5_Day_array_pro.js
@@ -0,0 +1,5 @@
+const namber = parseInt(prompt('Enter namber:'));
+for(let count = 1; count <= 10; count++ ){
+  const product = number * count;
+  console.log('${number} * ${count} = ${product}' );
+}
