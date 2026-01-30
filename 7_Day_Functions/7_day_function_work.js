9ae5f1270fd5375606178fa0da22615bba5db47a
filpos <mergetafilpos@gmail.com>
2026-01-30 19:39:53 +0300

    create 7_day_function_work.js message

--- 7_Day_Functions/7_day_function_work.js
+++ 7_Day_Functions/7_day_function_work.js
@@ -1,9 +1,24 @@
-function greet(name){
+/*function greet(name){
   console.log(`Hello, ${name}`);
 }
 greet("jhone");
+greet("filpos")
 // in by name 
 function greet(work){
   console.log(`Filpos, is, ${work}`)
 }
 greet("Priest")
+*/
+// Add two number in one function
+// function with two arguments
+function addNumbers(num1, num2) {
+    let sum = num1 + num2;
+   console.log(`Sum: ${sum}`);
+}
+addNumbers(10, 60);
+// Example 2
+function addNumbers(add1, add2 ){
+  let give = add1 + add2;
+  console.log(`Sum, ${give} `)
+}
+addNumbers(7, 9);
